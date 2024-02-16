f297a7b4b8eed93bba6e94c277ef047a95c1f7b2
Faiber abril <adrianfabril@gmail.com>
2024-02-16 11:31:03 -0500

    cambios

--- Automatizaciondeinforme.py
+++ Automatizaciondeinforme.py
@@ -75,30 +75,27 @@ documento_word.add_paragraph()
 dfcompetencias = df
 dfcompetencias = dfcompetencias[dfcompetencias['Competencia'] != '2 - RESULTADOS DE APRENDIZAJE ETAPA PRACTICA']
 grupos_competencia = dfcompetencias.groupby('Competencia')[['Competencia', 'Juicio de Evaluación']]
-dfcompetencias['Juicio de Evaluación'] = dfcompetencias['Juicio de Evaluación'].apply(lambda x: 'Sí' if x == 'APROBADO' else 'No' if x == 'POR EVALUAR' else x)
+dfcompetencias['Juicio de Evaluación'] = dfcompetencias['Juicio de Evaluación'].apply(lambda x: 'Si' if x == 'APROBADO' else 'No' if x == 'POR EVALUAR' else x)
 dfcompetencias = dfcompetencias.drop_duplicates(subset=['Competencia'])
 
 
 
 
 # Agregar una tabla al final del documento
-tablaCompetencias = documento_word.add_table(rows=len(dfestudiantesretiro) + 1, cols=3)  # +1 para incluir el encabezado
+tablaCompetencias = documento_word.add_table(rows=len(dfcompetencias) + 1, cols=2)  # +1 para incluir el encabezado
 tablaCompetencias.style = 'Table Grid'  # Aplicar un estilo de tabla
 
 # Agregar el encabezado de la tabla
 encabezado = tablaCompetencias.rows[0].cells
-encabezado[0].text = 'Nombre Completo'
-encabezado[1].text = 'Número de Documento'
-encabezado[2].text = 'Estado'
+encabezado[0].text = 'Competencias Desarrolladas'
+encabezado[1].text = 'Evaluado En SOFIAPLUS'
+
 
 # Agregar los datos de las columnas especificadas a la tabla
-for i, (_, fila) in enumerate(dfestudiantesretiro.iterrows(), start=1):
+for i, (_, fila) in enumerate(dfcompetencias.iterrows(), start=1):
     celdas = tablaCompetencias.rows[i].cells
-    celdas[0].text = fila['Nombrecompleto']
-    celdas[1].text = str(fila['Número de Documento'])  # Convertir a cadena si no es texto
-    celdas[2].text = fila['Estado']
-
-
+    celdas[0].text = fila['Competencia']
+    celdas[1].text = str(fila['Juicio de Evaluación'])  # Convertir a cadena si no es texto
 
 
 
@@ -106,7 +103,42 @@ for i, (_, fila) in enumerate(dfestudiantesretiro.iterrows(), start=1):
 # Agregar un párrafo en blanco entre las tablas
 documento_word.add_paragraph()
 
+# resultados y competencias sin evaluar
+dfcompetenciassinevaluar = df
+dfcompetenciassinevaluar = dfcompetenciassinevaluar[['Competencia','Resultado de Aprendizaje','Juicio de Evaluación']]
+dfcompetenciassinevaluar = dfcompetenciassinevaluar[dfcompetenciassinevaluar['Competencia'] != '2 - RESULTADOS DE APRENDIZAJE ETAPA PRACTICA']
+dfcompetenciassinevaluar['Juicio de Evaluación'] = dfcompetenciassinevaluar['Juicio de Evaluación'].apply(lambda x: 'Si' if x == 'APROBADO' else 'No' if x == 'POR EVALUAR' else x)
 
+
+# Supongamos que dfcompetenciassinevaluar es tu DataFrame original
+dfcompetenciassinevaluar = dfcompetenciassinevaluar.groupby('Resultado de Aprendizaje').agg({'Juicio de Evaluación': lambda x: (x == 'Si').sum(), 'Competencia': 'first'}).reset_index()
+
+# Ahora, si deseas filtrar las filas donde el conteo de 'Sí' es igual a cero y obtener las columnas 'Competencia' y 'Resultado de Aprendizaje':
+dfcompetenciassinevaluar = dfcompetenciassinevaluar[dfcompetenciassinevaluar['Juicio de Evaluación'] == 0][['Competencia', 'Resultado de Aprendizaje']]
+# Obtener los datos de las competencias y los resultados de aprendizaje
+dfcompetenciassinevaluar = dfcompetenciassinevaluar[['Competencia', 'Resultado de Aprendizaje']]
+
+# Agregar una tabla al final del documento
+tablaCompetenciassinevaluar = documento_word.add_table(rows=len(dfcompetenciassinevaluar) + 1, cols=2)  # +1 para incluir el encabezado
+tablaCompetenciassinevaluar.style = 'Table Grid'  # Aplicar un estilo de tabla
+
+# Agregar el encabezado de la tabla
+encabezado = tablaCompetenciassinevaluar.rows[0].cells
+encabezado[0].text = 'Competencia'
+encabezado[1].text = 'Resultado de Aprendizaje sin Evaluar'
+
+# Iterar sobre las filas de la tabla y los datos del DataFrame
+for i, (index, fila) in enumerate(dfcompetenciassinevaluar.iterrows(), start=1):
+    # Acceder a las celdas de la fila
+    celdas = tablaCompetenciassinevaluar.rows[i].cells
+    # Asignar los datos a las celdas
+    celdas[0].text = str(fila['Competencia'])
+    celdas[1].text = str(fila['Resultado de Aprendizaje'])
+
+
+
+# Agregar un párrafo en blanco entre las tablas
+documento_word.add_paragraph()
 
 
 
--- archivotemp.py
+++ archivotemp.py
@@ -6,16 +6,47 @@ from docx import Document
 documento_word = Document('acta.docx')
 
 # Cargar el archivo de Excel en competencias
-dfcompetencias = pd.read_excel('Reporte de Juicios Evaluativos (1).xls',header=0,skiprows=12)
-dfcompetencias = dfcompetencias[dfcompetencias['Competencia'] != '2 - RESULTADOS DE APRENDIZAJE ETAPA PRACTICA']
-grupos_competencia = dfcompetencias.groupby('Competencia')[['Competencia', 'Juicio de Evaluación']]
-dfcompetencias['Juicio de Evaluación'] = dfcompetencias['Juicio de Evaluación'].apply(lambda x: 'Sí' if x == 'APROBADO' else 'No' if x == 'POR EVALUAR' else x)
-dfcompetencias = dfcompetencias.drop_duplicates(subset=['Competencia'])
+dfcompetenciassinevaluar = pd.read_excel('Reporte de Juicios Evaluativos (1).xls',header=0,skiprows=12)
+dfcompetenciassinevaluar = dfcompetenciassinevaluar[['Competencia','Resultado de Aprendizaje','Juicio de Evaluación']]
+dfcompetenciassinevaluar = dfcompetenciassinevaluar[dfcompetenciassinevaluar['Competencia'] != '2 - RESULTADOS DE APRENDIZAJE ETAPA PRACTICA']
+dfcompetenciassinevaluar['Juicio de Evaluación'] = dfcompetenciassinevaluar['Juicio de Evaluación'].apply(lambda x: 'Si' if x == 'APROBADO' else 'No' if x == 'POR EVALUAR' else x)
 
 
+
+# Supongamos que dfcompetenciassinevaluar es tu DataFrame original
+dfcompetenciassinevaluar = dfcompetenciassinevaluar.groupby('Resultado de Aprendizaje').agg({'Juicio de Evaluación': lambda x: (x == 'Si').sum(), 'Competencia': 'first'}).reset_index()
+
+# Ahora, si deseas filtrar las filas donde el conteo de 'Sí' es igual a cero y obtener las columnas 'Competencia' y 'Resultado de Aprendizaje':
+dfcompetenciassinevaluar = dfcompetenciassinevaluar[dfcompetenciassinevaluar['Juicio de Evaluación'] == 0][['Competencia', 'Resultado de Aprendizaje']]
+
+dfcompetenciassinevaluar  = dfcompetenciassinevaluar[['Competencia','Resultado de Aprendizaje']]
+
+
+print(dfcompetenciassinevaluar)
+
+
+
+"""
+
+dfcompetenciassinevaluar = dfcompetenciassinevaluar[dfcompetenciassinevaluar['Juicio de Evaluación'] == 'No']
+dfcompetenciassinevaluar['Juicio de Evaluación'] = dfcompetenciassinevaluar.apply(lambda row: 'Sí' if not pd.isnull(row['Resultado de Aprendizaje']) else 'No', axis=1)
+
+
+conteo_si = dfcompetenciassinevaluar.groupby('Resultado de Aprendizaje')['Juicio de Evaluación'].apply(lambda x: (x == 'Si').sum()).reset_index()
+conteo_no = dfcompetenciassinevaluar.groupby('Resultado de Aprendizaje')['Juicio de Evaluación'].apply(lambda x: (x == 'No').sum()).reset_index()
+
+documentos_con_mas_de_un_no = ['Competencia'] & conteo_no[conteo_no['Juicio de Evaluación'] == 0 ]['Resultado de Aprendizaje']
+
+
+
+documentos_con_mas_de_un_si = conteo_si[conteo_si['Juicio de Evaluación'] > 1]['Resultado de Aprendizaje']
+
+dfcompetenciassinevaluar = dfcompetenciassinevaluar.apply(lambda row: 'No' if row['Resultado de Aprendizaje'] in documentos_con_mas_de_un_si else row['Juicio de Evaluación'], axis=1)
 
 
 
 
 # Mostrar el DataFrame resultante
-print(dfcompetencias[['Competencia', 'Juicio de Evaluación']])
+#print(dfcompetencias[['Competencia', 'Juicio de Evaluación']])"""
+
+

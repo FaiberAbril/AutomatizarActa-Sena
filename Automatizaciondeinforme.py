e204afe7cbfe997cd0353de901069270f320df62
Faiber abril <adrianfabril@gmail.com>
2024-02-16 12:03:35 -0500

    todas las tablas finalizadas

--- Automatizaciondeinforme.py
+++ Automatizaciondeinforme.py
@@ -142,6 +142,67 @@ documento_word.add_paragraph()
 
 
 
+
+horas_planeacion = {
+    'Utilizar herramientas informáticas de acuerdo con las necesidades de manejo de información': 48,
+    'APLICAR PRÁCTICAS DE PROTECCIÓN AMBIENTAL, SEGURIDAD Y SALUD EN EL TRABAJO DE ACUERDO CON LAS POLÍTICAS ORGANIZACIONALES Y LA NORMATIVIDAD VIGENTE.': 48,
+    'Razonar cuantitativamente frente a situaciones susceptibles de ser abordadas de manera matemática en contextos laborales, sociales y personales.': 48,
+    'APLICACIÓN DE CONOCIMIENTOS DE LAS CIENCIAS NATURALES DE ACUERDO CON SITUACIONES DEL CONTEXTO PRODUCTIVO Y SOCIAL.': 48,
+    'DESARROLLAR PROCESOS DE COMUNICACIÓN EFICACES Y EFECTIVOS, TENIENDO EN CUENTA SITUACIONES  DE ORDEN SOCIAL, PERSONAL Y PRODUCTIVO.': 48,
+    'GENERAR HÁBITOS SALUDABLES DE VIDA MEDIANTE LA APLICACIÓN DE PROGRAMAS DE ACTIVIDAD FÍSICA EN LOS CONTEXTOS PRODUCTIVOS Y SOCIALES.': 48,
+    'Orientar investigación formativa según referentes técnicos': 48,
+    'Enrique Low Murtra-Interactuar en el contexto productivo y social de acuerdo con principios  éticos para la construcción de una cultura de paz.': 48,
+    'INTERACTUAR EN LENGUA INGLESA DE FORMA ORAL Y ESCRITA DENTRO DE CONTEXTOS SOCIALES Y LABORALES SEGÚN LOS CRITERIOS ESTABLECIDOS POR EL MARCO COMÚN EUROPEO DE REFERENCIA PARA LAS LENGUAS.': 384 ,
+    'Fomentar cultura emprendedora según habilidades y competencias personales': 48
+}
+
+
+
+
+# Cargar el archivo de Excel en competencias
+dfinstructorhoras = pd.read_excel('Reporte de Instructores por Ficha.xls',header=0,skiprows=10)
+
+
+dfinstructorhoras['Nombrecompleto'] = dfinstructorhoras['Nombre Instructor'] + ' ' + dfinstructorhoras['Apellido Instructor']
+dfinstructorhoras = dfinstructorhoras.sort_values(by='Competencia', ascending=True)
+
+# Añadir la columna 'Horas Planeadas' al DataFrame
+dfinstructorhoras['Horas Planeadas'] = dfinstructorhoras['Competencia'].map(horas_planeacion)
+
+dfinstructorhoras = dfinstructorhoras[['Nombrecompleto','Estado Instructor','Competencia','Horas Programadas','Horas Planeadas']]
+
+
+# Agregar una tabla al final del documento
+tablainstructores = documento_word.add_table(rows=len(dfinstructorhoras) + 1, cols=5)  # +1 para incluir el encabezado
+tablainstructores.style = 'Table Grid'  # Aplicar un estilo de tabla
+
+# Agregar el encabezado de la tabla
+encabezado = tablainstructores.rows[0].cells
+encabezado[0].text = 'Nombre completo Instructor'
+encabezado[1].text = 'Estado Instructor'
+encabezado[2].text = 'Competencia'
+encabezado[3].text = 'Horas Programadas en sofia plus'
+encabezado[4].text = 'Horas Planeacion'
+
+# Iterar sobre las filas de la tabla y los datos del DataFrame
+for i, (index, fila) in enumerate(dfinstructorhoras.iterrows(), start=1):
+    # Acceder a las celdas de la fila
+    celdas = tablainstructores.rows[i].cells
+    # Asignar los datos a las celdas
+    celdas[0].text = str(fila['Nombrecompleto'])
+    celdas[1].text = str(fila['Estado Instructor'])
+    celdas[2].text = str(fila['Competencia'])
+    celdas[3].text = str(fila['Horas Programadas'])
+    celdas[4].text = str(fila['Horas Planeadas'])
+
+
+
+
+
+# Agregar un párrafo en blanco entre las tablas
+documento_word.add_paragraph()
+
+
 documento_word.save('acta.docx')
 
 print("Se ha agregado una tabla al final del documento de Word con los datos de las columnas especificadas.")
--- archivotemp.py
+++ archivotemp.py
@@ -3,50 +3,42 @@ from docx import Document
 
 
 # Abrir el documento Word existente
-documento_word = Document('acta.docx')
+
+horas_planeacion = {
+    'Utilizar herramientas informáticas de acuerdo con las necesidades de manejo de información': 48,
+    'APLICAR PRÁCTICAS DE PROTECCIÓN AMBIENTAL, SEGURIDAD Y SALUD EN EL TRABAJO DE ACUERDO CON LAS POLÍTICAS ORGANIZACIONALES Y LA NORMATIVIDAD VIGENTE.': 48,
+    'Razonar cuantitativamente frente a situaciones susceptibles de ser abordadas de manera matemática en contextos laborales, sociales y personales.': 48,
+    'APLICACIÓN DE CONOCIMIENTOS DE LAS CIENCIAS NATURALES DE ACUERDO CON SITUACIONES DEL CONTEXTO PRODUCTIVO Y SOCIAL.': 48,
+    'DESARROLLAR PROCESOS DE COMUNICACIÓN EFICACES Y EFECTIVOS, TENIENDO EN CUENTA SITUACIONES  DE ORDEN SOCIAL, PERSONAL Y PRODUCTIVO.': 48,
+    'GENERAR HÁBITOS SALUDABLES DE VIDA MEDIANTE LA APLICACIÓN DE PROGRAMAS DE ACTIVIDAD FÍSICA EN LOS CONTEXTOS PRODUCTIVOS Y SOCIALES.': 48,
+    'Orientar investigación formativa según referentes técnicos': 48,
+    'Enrique Low Murtra-Interactuar en el contexto productivo y social de acuerdo con principios  éticos para la construcción de una cultura de paz.': 48,
+    'INTERACTUAR EN LENGUA INGLESA DE FORMA ORAL Y ESCRITA DENTRO DE CONTEXTOS SOCIALES Y LABORALES SEGÚN LOS CRITERIOS ESTABLECIDOS POR EL MARCO COMÚN EUROPEO DE REFERENCIA PARA LAS LENGUAS.': 384 ,
+    'Fomentar cultura emprendedora según habilidades y competencias personales': 48
+}
+
+
+
 
 # Cargar el archivo de Excel en competencias
-dfcompetenciassinevaluar = pd.read_excel('Reporte de Juicios Evaluativos (1).xls',header=0,skiprows=12)
-dfcompetenciassinevaluar = dfcompetenciassinevaluar[['Competencia','Resultado de Aprendizaje','Juicio de Evaluación']]
-dfcompetenciassinevaluar = dfcompetenciassinevaluar[dfcompetenciassinevaluar['Competencia'] != '2 - RESULTADOS DE APRENDIZAJE ETAPA PRACTICA']
-dfcompetenciassinevaluar['Juicio de Evaluación'] = dfcompetenciassinevaluar['Juicio de Evaluación'].apply(lambda x: 'Si' if x == 'APROBADO' else 'No' if x == 'POR EVALUAR' else x)
+dfinstructorhoras = pd.read_excel('Reporte de Instructores por Ficha.xls',header=0,skiprows=10)
 
 
+dfinstructorhoras['Nombrecompleto'] = dfinstructorhoras['Nombre Instructor'] + ' ' + dfinstructorhoras['Apellido Instructor']
+dfinstructorhoras = dfinstructorhoras.sort_values(by='Competencia', ascending=True)
 
-# Supongamos que dfcompetenciassinevaluar es tu DataFrame original
-dfcompetenciassinevaluar = dfcompetenciassinevaluar.groupby('Resultado de Aprendizaje').agg({'Juicio de Evaluación': lambda x: (x == 'Si').sum(), 'Competencia': 'first'}).reset_index()
+# Añadir la columna 'Horas Planeadas' al DataFrame
+dfinstructorhoras['Horas Planeadas'] = dfinstructorhoras['Competencia'].map(horas_planeacion)
 
-# Ahora, si deseas filtrar las filas donde el conteo de 'Sí' es igual a cero y obtener las columnas 'Competencia' y 'Resultado de Aprendizaje':
-dfcompetenciassinevaluar = dfcompetenciassinevaluar[dfcompetenciassinevaluar['Juicio de Evaluación'] == 0][['Competencia', 'Resultado de Aprendizaje']]
-
-dfcompetenciassinevaluar  = dfcompetenciassinevaluar[['Competencia','Resultado de Aprendizaje']]
-
-
-print(dfcompetenciassinevaluar)
-
-
-
-"""
-
-dfcompetenciassinevaluar = dfcompetenciassinevaluar[dfcompetenciassinevaluar['Juicio de Evaluación'] == 'No']
-dfcompetenciassinevaluar['Juicio de Evaluación'] = dfcompetenciassinevaluar.apply(lambda row: 'Sí' if not pd.isnull(row['Resultado de Aprendizaje']) else 'No', axis=1)
-
-
-conteo_si = dfcompetenciassinevaluar.groupby('Resultado de Aprendizaje')['Juicio de Evaluación'].apply(lambda x: (x == 'Si').sum()).reset_index()
-conteo_no = dfcompetenciassinevaluar.groupby('Resultado de Aprendizaje')['Juicio de Evaluación'].apply(lambda x: (x == 'No').sum()).reset_index()
-
-documentos_con_mas_de_un_no = ['Competencia'] & conteo_no[conteo_no['Juicio de Evaluación'] == 0 ]['Resultado de Aprendizaje']
-
-
-
-documentos_con_mas_de_un_si = conteo_si[conteo_si['Juicio de Evaluación'] > 1]['Resultado de Aprendizaje']
-
-dfcompetenciassinevaluar = dfcompetenciassinevaluar.apply(lambda row: 'No' if row['Resultado de Aprendizaje'] in documentos_con_mas_de_un_si else row['Juicio de Evaluación'], axis=1)
+dfinstructorhoras = dfinstructorhoras[['Nombrecompleto','Estado Instructor','Competencia','Horas Programadas','Horas Planeadas']]
 
 
 
 
-# Mostrar el DataFrame resultante
+print(dfinstructorhoras)
+
+
+
 #print(dfcompetencias[['Competencia', 'Juicio de Evaluación']])"""
 
 
